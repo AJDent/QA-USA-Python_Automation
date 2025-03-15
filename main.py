594ff7b8a0beb0b6af08c3ea5a7c5cbd875789aa
ajdent <adarrylldentjr@gmail.com>
2025-03-14 20:00:25 -0400

    Project 8

--- main.py
+++ main.py
@@ -1,2 +1,124 @@
+import time
+
+from selenium import webdriver
+import data
+import helpers
+import pages
+
 
 class TestUrbanRoutes:
+    @classmethod
+    def setup_class(cls):
+        # do not modify - we need additional logging enabled in order to retrieve phone confirmation code
+        from selenium.webdriver import DesiredCapabilities
+        capabilities = DesiredCapabilities.CHROME
+        capabilities["goog:loggingPrefs"] = {'performance': 'ALL'}
+        cls.driver = webdriver.Chrome()
+        if helpers.is_url_reachable(data.URBAN_ROUTES_URL):
+                print("Connected to the Urban Routes server")
+        else:
+                print("Cannot connect to Urban Routes. Check the server is on and still running")
+
+    def test_set_route(self):
+        #setting the address
+        self.driver.get(data.URBAN_ROUTES_URL)
+        page = pages.UrbanRoutesPage(self.driver)
+        page.set_addresses(data.ADDRESS_FROM, data.ADDRESS_TO)
+        actual_value = data.ADDRESS_FROM
+        actual_value_1 = data.ADDRESS_TO
+        expected_value = "East 2nd Street, 601"
+        expected_value_1 = "1300 1st St"
+        assert expected_value in actual_value, f"Expected '{expected_value}', but got '{actual_value}'"
+        assert expected_value_1 in actual_value_1, f"Expected '{expected_value_1}', but got '{actual_value_1}'"
+
+    def test_select_plan(self):
+        #selecting the plan
+        self.driver.get(data.URBAN_ROUTES_URL)
+        page = pages.UrbanRoutesPage(self.driver)
+        page.set_addresses(data.ADDRESS_FROM, data.ADDRESS_TO)
+        page.selecting_supportive_plan()
+        actual_value = page.asserting_supportive()
+        expected_value = "Supportive"
+        assert expected_value in actual_value, f"Expected '{expected_value}', but got '{actual_value}'"
+
+
+
+    def test_fill_phone_number(self):
+        #entering and verifying the phone number
+        self.driver.get(data.URBAN_ROUTES_URL)
+        page = pages.UrbanRoutesPage(self.driver)
+        page.set_addresses(data.ADDRESS_FROM, data.ADDRESS_TO)
+        page.selecting_supportive_plan()
+        page.filling_in_the_phone_number(data.PHONE_NUMBER)
+        actual_value = page.assert_phone_number()
+        expected_value = "+1 123 123 12 12"
+        assert expected_value in actual_value, f"Expected '{expected_value}', but got '{actual_value}'"
+
+    def test_fill_card(self):
+        #entering card info
+        self.driver.get(data.URBAN_ROUTES_URL)
+        page = pages.UrbanRoutesPage(self.driver)
+        page.set_addresses(data.ADDRESS_FROM, data.ADDRESS_TO)
+        page.adding_a_credit_card(data.CARD_NUMBER, data.CARD_CODE)
+        assert page.assert_card()
+
+    def test_comment_for_driver(self):
+        #leaving a comment for the driver
+        self.driver.get(data.URBAN_ROUTES_URL)
+        page = pages.UrbanRoutesPage(self.driver)
+        page.set_addresses(data.ADDRESS_FROM, data.ADDRESS_TO)
+        page.writing_a_comment_for_the_driver(data.MESSAGE_FOR_DRIVER)
+        time.sleep(2)
+        actual_value = page.assert_comment_driver()
+        expected_value = "Stop at the juice bar, please"
+        assert expected_value in actual_value, f"Expected '{expected_value}', but got '{actual_value}'"
+
+    def test_order_blanket_and_handkerchiefs(self):
+        #verifiying the slide works
+        self.driver.get(data.URBAN_ROUTES_URL)
+        page = pages.UrbanRoutesPage(self.driver)
+        page.set_addresses(data.ADDRESS_FROM, data.ADDRESS_TO)
+        page.selecting_supportive_plan()
+        page.ordering_blanket_and_handkerchiefs()
+        assert page.blanket_handkerchiefs_check()
+
+    def test_order_2_ice_creams(self):
+        #verifying that 2 ice creams are chosen
+        self.driver.get(data.URBAN_ROUTES_URL)
+        page = pages.UrbanRoutesPage(self.driver)
+        page.set_addresses(data.ADDRESS_FROM, data.ADDRESS_TO)
+        page.selecting_supportive_plan()
+        page.ordering_ice_cream()
+        actual_value = page.assert_ice_cream()
+        expected_value = "2"
+        assert expected_value in actual_value, f"Expected '{expected_value}', but got '{actual_value}'"
+
+
+    def test_car_search_model_appears(self):
+        #verifying that the car search is activated
+        self.driver.get(data.URBAN_ROUTES_URL)
+        page = pages.UrbanRoutesPage(self.driver)
+        page.set_addresses(data.ADDRESS_FROM, data.ADDRESS_TO)
+        page.selecting_supportive_plan()
+        page.writing_a_comment_for_the_driver(data.MESSAGE_FOR_DRIVER)
+        page.booking_taxi()
+        actual_value = page.assert_taxi_order()
+        expected_value = "Car search"
+        assert expected_value in actual_value, f"Expected '{expected_value}', but got '{actual_value}'"
+
+
+
+# ✅ Ensure this code is OUTSIDE the class (after its definition)
+if __name__ == "__main__":
+    test = TestUrbanRoutes()  # Create an instance of the class
+    test.test_set_route()
+    test.test_select_plan()
+    test.test_fill_phone_number()
+    test.test_fill_card()
+    test.test_comment_for_driver()
+    test.test_order_blanket_and_handkerchiefs()
+    test.test_order_2_ice_creams()
+
+@classmethod
+def teardown_class(cls):
+    cls.driver.quit()
---
+++ pages.py
@@ -0,0 +1,111 @@
+from selenium.webdriver.common.by import By
+from helpers import retrieve_phone_code
+
+
+
+class UrbanRoutesPage:
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    FROM_LOCATOR = (By.ID, 'from')
+    TO_LOCATOR = (By.ID, 'to')
+    CALL_TAXI_BUTTON = (By.XPATH, '//button[@class= "button round"]')
+    SUPPORTIVE_ICON_LOCATOR = (By. XPATH, '// div[contains(@class, "tcard")][5]')
+    ASSERT_SUPPORT_TITLE = (By.XPATH, '(//div[@class="tcard-title"])[5]')
+    PHONE_NUMBER_BOX = (By.XPATH, '//div[@class="np-button"]')
+    PHONE_NUMBER_INPUT = (By.ID, 'phone')
+    PHONE_BUTTON = (By.XPATH, '(//button[@class="button full"])[1]')
+    SMS_CODE_LOCATOR = (By.ID, 'code')
+    SMS_BUTTON = (By.XPATH, '(//button[@class="button full"])[2]')
+    PHONE_NUMBER_BOX_ASSERT = (By.XPATH, '//div[@class="np-text"]')
+    CREDIT_CARD_LOCATOR = (By.XPATH, '//div[@class="pp-button filled"]')
+    CREDIT_CARD_OPTION = (By.XPATH, '//div[@class="pp-row disabled"]')
+    CREDIT_CARD_NUMBER = (By.ID, 'number')
+    CREDIT_CARD_CODE = (By.XPATH, '(//input[@class="card-input"])[2]')
+    CARD_OUTSIDE_BOX = (By.XPATH, '//div[@class="card-wrapper"]')
+    CARD_LINK_BUTTON = (By.XPATH, '(//button[@class="button full"])[4]')
+    ASSERT_CARD = (By.ID, 'card-1')
+    COMMENT_FOR_DRIVER = (By.ID, 'comment')
+    ASSERT_COMMENT = (By.XPATH, '(//input[@class="input"])[5]')
+    BLANKET_HANDKERCHIEFS_ON = (By.XPATH, '//div[@class="r-sw"]')
+    ICE_CREAM_COUNTER = (By.XPATH, '//div[@class="counter-plus"]')
+    SMART_BUTTON_TO_BOOK = (By.XPATH, '//button[@class="smart-button"]')
+    BLANKET_HANDKERCHIEFS_CHECK = (By.XPATH, '//input[@class="switch-input"]')
+    ASSERT_ICE_CREAM = (By.XPATH, '//div[@class="counter-value"]')
+    ASSERT_ORDER = (By.XPATH, '//div[@class="order-header-title"]')
+
+
+
+    def set_addresses(self, from_address, to_address):
+        #This is setting the address in the beginning
+        self.driver.find_element(*self.FROM_LOCATOR).send_keys(from_address)
+        self.driver.find_element(*self.TO_LOCATOR).send_keys(to_address)
+        self.driver.find_element(*self.CALL_TAXI_BUTTON).click()
+
+    def selecting_supportive_plan(self):
+        #This selects the supportive car option
+        self.driver.find_element(*self.SUPPORTIVE_ICON_LOCATOR).click()
+
+    def filling_in_the_phone_number(self, phone_number):
+        #Filling in the phone number inbox
+        self.driver.find_element(*self.PHONE_NUMBER_BOX).click()
+        self.driver.find_element(*self.PHONE_NUMBER_INPUT).send_keys(phone_number)
+        self.driver.find_element(*self.PHONE_BUTTON).click()
+        self.driver.find_element(*self.SMS_CODE_LOCATOR).send_keys(retrieve_phone_code(self.driver))
+        self.driver.find_element(*self.SMS_BUTTON).click()
+
+    def adding_a_credit_card(self, card_number, card_code):
+        #This sequence adds an credit card
+        self.driver.find_element(*self.CREDIT_CARD_LOCATOR).click()
+        self.driver.find_element(*self.CREDIT_CARD_OPTION).click()
+        self.driver.find_element(*self.CREDIT_CARD_NUMBER).send_keys(card_number)
+        self.driver.find_element(*self.CREDIT_CARD_CODE).send_keys(card_code)
+        self.driver.find_element(*self.CARD_OUTSIDE_BOX).click()
+        self.driver.find_element(*self.CARD_LINK_BUTTON).click()
+
+    def writing_a_comment_for_the_driver(self, message_for_driver):
+        #This code creates a comment for the driver
+        self.driver.find_element(*self.COMMENT_FOR_DRIVER).send_keys(message_for_driver)
+
+    def assert_comment_driver(self):
+        return self.driver.find_element(*self.ASSERT_COMMENT).get_attribute("value")
+
+
+    def ordering_blanket_and_handkerchiefs(self):
+        #ordering the blanket and handkerchiefs
+        self.driver.find_element(*self.BLANKET_HANDKERCHIEFS_ON).click()
+
+    def blanket_handkerchiefs_check(self):
+        return self.driver.find_element(*self.BLANKET_HANDKERCHIEFS_CHECK).get_property("checked")
+
+
+    def ordering_ice_cream(self):
+        #orderes ice cream
+        self.driver.find_element(*self.ICE_CREAM_COUNTER).click()
+        self.driver.find_element(*self.ICE_CREAM_COUNTER).click()
+
+    def assert_ice_cream(self):
+        return self.driver.find_element(*self.ASSERT_ICE_CREAM).text
+
+
+    def booking_taxi(self):
+        #booking taxi
+        self.driver.find_element(*self.SMART_BUTTON_TO_BOOK).click()
+
+    def asserting_supportive(self):
+        return self.driver.find_element(*self.ASSERT_SUPPORT_TITLE).text
+
+    def assert_phone_number(self):
+        return self.driver.find_element(*self.PHONE_NUMBER_BOX_ASSERT).text
+
+    def assert_card(self):
+        return self.driver.find_element(*self.ASSERT_CARD).get_property("checked")
+
+    def assert_taxi_order(self):
+        return self.driver.find_element(*self.ASSERT_ORDER).text
+
+
+
+
+
